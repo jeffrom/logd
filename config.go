0f3112fb984853a5265d74f41dbf6f47f8241562
Jeff Martin <jeffmartin@gmail.com>
2018-02-10 14:54:38 -0500

    clean up json config fields

--- config.go
+++ config.go
@@ -4,22 +4,24 @@ import "encoding/json"
 
 // Config holds configuration variables
 type Config struct {
-	Verbose             bool
-	CanShutdown         bool
+	Verbose             bool `json:"verbose"`
+	CanShutdown         bool `json:"can_shutdown"`
 	Logger              Logger
-	Hostport            string
-	ServerTimeout       uint
-	ClientTimeout       uint
-	LogFile             string
-	LogFileMode         int
-	MaxChunkSize        int
-	PartitionSize       int
-	MaxPartitions       int
-	PartitionDeleteHook string
-	IndexCursorSize     uint64
-	StartID             uint64
-	ReadLimit           uint64
-	ReadForever         bool
+	Hostport            string `json:"host"`
+	ServerTimeout       uint   `json:"server_timeout"`
+	ClientTimeout       uint   `json:"client_timeout"`
+	LogFile             string `json:"log_file"`
+	LogFileMode         int    `json:"log_file_mode"`
+	MaxChunkSize        int    `json:"max_chunk_size"`
+	PartitionSize       int    `json:"partition_size"`
+	MaxPartitions       int    `json:"max_partitions"`
+	PartitionDeleteHook string `json:"partition_delete_hook"`
+	IndexCursorSize     uint64 `json:"index_cursor_size"`
+
+	// client configs
+	StartID     uint64 `json:"start"`
+	ReadLimit   uint64 `json:"limit"`
+	ReadForever bool   `json:"forever"`
 }
 
 // NewConfig returns a new configuration object
