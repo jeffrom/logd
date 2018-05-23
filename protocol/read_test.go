a460b3f029d791548f526d03f96c71055f3b9cd4
Jeff Martin <jeffmartin@gmail.com>
2018-05-22 21:21:21 -0400

    add a READ test
    Squashed commit of the following:
    
    commit 7dc41d2bfc6da902110da387426760865c1e08c1
    Author: Jeff Martin <jeffmartin@gmail.com>
    Date:   Tue May 22 21:19:33 2018 -0400
    
        add a read test

---
+++ protocol/read_test.go
@@ -0,0 +1,22 @@
+package protocol
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/jeffrom/logd/testhelper"
+)
+
+func TestWriteRead(t *testing.T) {
+	conf := testhelper.TestConfig(testing.Verbose())
+	read := NewRead(conf)
+	read.Offset = 1234567
+	read.Messages = 100
+
+	b := &bytes.Buffer{}
+	if _, err := read.WriteTo(b); err != nil {
+		t.Fatalf("unexpected error writing READ request: %v", err)
+	}
+
+	testhelper.CheckGoldenFile("read.simple", b.Bytes(), testhelper.Golden)
+}
