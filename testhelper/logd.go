f24ec1c5583be3b8df11792ec2d0a59d98380729
Jeff Martin <jeffmartin@gmail.com>
2018-09-30 17:27:55 -0400

    tweak IsCi check

--- testhelper/logd.go
+++ testhelper/logd.go
@@ -89,7 +89,7 @@ func Repeat(s string, n int) string {
 }
 
 func IsCI() bool {
-	return os.Getenv("CI") == "true"
+	return os.Getenv("CI") != ""
 }
 
 // FailingWriter fails the first call to Write, then the second, then third,
