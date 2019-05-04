7f69b56323a922924d88360a28dd06359e1ea55f
Jeff Martin <jeffmartin@gmail.com>
2019-05-04 16:30:49 -0400

    make sure we have benchcmp

--- script/compare_benchmarks.sh
+++ script/compare_benchmarks.sh
@@ -3,6 +3,10 @@ set -euxo pipefail
 
 cd "$( cd "$(dirname "$0")" ; pwd )/../"
 
+if ! command -v benchcmp; then
+    GO111MODULE=off go get golang.org/x/tools/cmd/benchcmp
+fi
+
 # TODO maybe should check if we're using a volume too
 if grep "docker" /proc/1/cgroup > /dev/null; then
     echo "in a container, so cleaning git state. These changes will be undone:"
@@ -43,5 +47,6 @@ trap finish EXIT
 # NOTE the first argument is the output of the SECOND most recent commit.
 # that means, if on master, the second most recent commit in master. For all
 # other branches, it means HEAD on master. Basically, the "new" column will be
-# the branch you just pushed.
+# the branch you just pushed, but the benchmarks run on the new branch first.
+# This seemed nicest because if the benchmarks fail, you know immediately.
 benchcmp report/bench.out report/bench.out.1 | tee -a report/benchcmp.out
