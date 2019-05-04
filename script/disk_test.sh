73d86ebbbfc72dc51bd9c845ba712146ebca335c
Jeff Martin <jeffmartin@gmail.com>
2019-05-04 00:06:13 -0400

    didn't mean to remove this

---
+++ script/disk_test.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#
+# setup script for device mapper based test suite.
+#
+# this script sets up and tears down filesystems using device mapper drivers
+# intended for testing, including:
+#   - error
+#   - flakey
+#   - throttle
+#   - delay
+#   - zero ?
+#   - rwsplit ?
+#
+# TODO dmsetup 8)
+# https://github.com/goldwynr/xfstests/blob/00e3549c659cdc0b82adf1010dbbf4be7d9db411/common/dmflakey
+#
+
+set -euxo pipefail
+
+echo "not implemented"
+exit 1
