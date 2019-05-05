3d01f8b1a8f45a7ac91f5e063b4c54b24f5fe2df
Jeff Martin <jeffmartin@gmail.com>
2019-05-05 04:05:34 -0400

    chore: change semver script

--- script/bump-version.sh
+++ script/bump-version.sh
@@ -2,12 +2,12 @@
 set -euo pipefail
 
 # Usage: ./script/bump-version.sh [patch|minor|major]
-# see git-semver options
+# see semver options
 
 cd "$( cd "$(dirname "$0")" ; pwd )/../"
 
-if ! command -v git-semver > /dev/null; then
-    echo "git-semver is required: https://github.com/markchalloner/git-semver"
+if ! command -v semver > /dev/null; then
+    echo "semver-tool is required: https://github.com/fsaintjacques/semver-tool"
     exit 1
 fi
 
@@ -34,33 +34,46 @@ if [[ "$release" != "true" ]]; then
     echo "Use RELEASE=true $0 to commit."
 fi
 
+# get latest version
+latest=$(git tag  -l --merged master --sort='-*authordate' | head -n1)
+latest=$(git tag  -l --merged master --sort='-*authordate' | head -n1)
+semver_parts=(${latest//./ })
+major=${semver_parts[0]}
+minor=${semver_parts[1]}
+patch=${semver_parts[2]}
+current_version=${major}.${minor}.${patch}
+
+echo "Current version is $current_version"
+
 # update VERSION file
 args=($@)
 if [[ ${#args[@]} -eq 0 ]]; then
     # shortsha=$(git rev-parse --short=9 --verify HEAD)
-    next=$(git-semver patch --dryrun)
+    next=$(semver bump patch "$current_version")
 else
-    next=$(git-semver "$@" --dryrun)
+    next=$(semver bump "$@" "$current_version")
 fi
 
 version="$(echo "$next" | sed -e 's/^v//' | tr -d '\n')"
-printf "%s" "$version" > VERSION
 
 # generate CHANGELOG
 changelog=$(./script/generate-changelog.sh --config .chglog/config-commit.yml --next-tag v"$version" v"$version")
-
-set -x
-./script/generate-changelog.sh --output CHANGELOG.md --next-tag v"$version"
-set +x
 
 commit_message="logd v$version
 $changelog
 "
 
 if [[ "$release" != "true" ]]; then
+    echo "VERSION: $version"
     echo "commit: $commit_message"
     exit
 fi
+
+printf "%s" "$version" > VERSION
+
+set -x
+./script/generate-changelog.sh --output CHANGELOG.md --next-tag v"$version"
+set +x
 
 git commit VERSION CHANGELOG.md --allow-empty -t <(echo -n "$commit_message")
 git tag -a v"$version" -m "logd v$version"
